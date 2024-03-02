9329d394814d3a0acf6aa64fa34b158f9050281b
YoNightFury <abhilashmaniyar70@gmail.com>
2024-03-02 14:59:54 +0530

    file download

--- src/main/java/com/demo/controllers/FileDemoController.java
+++ src/main/java/com/demo/controllers/FileDemoController.java
@@ -2,8 +2,15 @@ package com.demo.controllers;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +28,22 @@ public class FileDemoController {
 		return ResponseEntity.ok("file saved");
 		}
 		catch(IOException ioex){
+			return ResponseEntity.internalServerError().body(ioex.getMessage());
+		}
+	}
+	
+	
+	@GetMapping("{fileName}")
+	public ResponseEntity<?> downloadImage(@PathVariable("fileName") String fileName){
+		String path = System.getProperty("user.home")+"/"+fileName;
+		Path p = Paths.get(path);
+		try {
+			byte[] fileByteArr = Files.readAllBytes(p);
+			return ResponseEntity
+					.status(HttpStatus.OK) // set status as OK
+					.contentType(MediaType.IMAGE_JPEG) // response Content-Type = image/jpeg
+					.body(fileByteArr); // body is the image byte array
+		}catch(IOException ioex) {
 			return ResponseEntity.internalServerError().body(ioex.getMessage());
 		}
 	}
