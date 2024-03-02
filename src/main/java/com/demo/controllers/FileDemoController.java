f5ca9abf0480349f9b99956ce2cf847355f071a8
YoNightFury <abhilashmaniyar70@gmail.com>
2024-03-02 14:48:16 +0530

    added file upload method in the controller Go to your thunder client or postman Select POST Method Go to form section tick mark allow file name: file value: select a file send the request Go to you home directory and check that the file was created there

--- src/main/java/com/demo/controllers/FileDemoController.java
+++ src/main/java/com/demo/controllers/FileDemoController.java
@@ -1,8 +1,27 @@
 package com.demo.controllers;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class FileDemoController {
 
+	@PostMapping("/")
+	public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file){
+		String path = System.getProperty("user.home")+"/"+file.getOriginalFilename();
+		File f = new File(path);
+		try {
+		file.transferTo(f);
+		return ResponseEntity.ok("file saved");
+		}
+		catch(IOException ioex){
+			return ResponseEntity.internalServerError().body(ioex.getMessage());
+		}
+	}
 }
